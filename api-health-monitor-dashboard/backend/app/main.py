809e62c4b9e8963891f73a560e1803a366119165
Ashish Parimoo <ashish.parimoo@rockwellcollins.com>
2021-01-02 18:46:09 +0000

    add Dockerfile and update main script

--- api-health-monitor-dashboard/backend/app/main.py
+++ api-health-monitor-dashboard/backend/app/main.py
@@ -74,6 +74,3 @@ async def get_api_health_status(service: str, settings: config.Settings = Depend
     except StopIteration:
         return {'error': f"{service} doesn't exist"}
 
-
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=get_settings().health_monitor_api_port)
